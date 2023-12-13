2e6cd910e24c32cda9a628d532b3de5e9a6e5b9d
jannet18 <janetngei18@gmail.com>
2023-12-14 02:28:11 +0300

    user storage in database complete

--- api/index.js
+++ api/index.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const User = require("./models/user");
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/register", (req, res) => {
+mongoose.connect(
+  "mongodb+srv://jannylynemiaz:s1kKMGLLnjgl1EIV@cluster0.zpvtryi.mongodb.net/?retryWrites=true&w=majority"
+);
+app.post("/register", async (req, res) => {
+  // Create user
   const { username, password } = req.body;
-  res.json({ requestData: { username, password } });
+  try {
+    const userDoc = await User.create({ username, password });
+    res.json(userDoc);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 app.listen(4000);
+// p8EkIgxY3F5bDYIL
+// s1kKMGLLnjgl1EIV
+// mongodb+srv://jannylynemiaz:s1kKMGLLnjgl1EIV@cluster0.zpvtryi.mongodb.net/?retryWrites=true&w=majority
