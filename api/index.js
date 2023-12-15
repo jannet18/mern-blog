958b8a52baf8019e97da292067cf3d2642d1dbea
jannet18 <janetngei18@gmail.com>
2023-12-15 14:06:30 +0300

    registration and login

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ const app = express();
 const cors = require("cors");
 const { default: mongoose } = require("mongoose");
 const User = require("./models/user");
+const bcrypt = require("bcryptjs");
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors());
 app.use(express.json());
@@ -14,12 +17,24 @@ app.post("/register", async (req, res) => {
   // Create user
   const { username, password } = req.body;
   try {
-    const userDoc = await User.create({ username, password });
+    const userDoc = await User.create({
+      username,
+      password: bcrypt.hashSync(password, salt),
+    });
     res.json(userDoc);
   } catch (error) {
     res.status(400).json(error);
   }
 });
+
+// user login
+app.post("/login"),
+  async (req, res) => {
+    const { username } = req.body;
+    const userDoc = await User.findOne({ username: username });
+    res.json(userDoc);
+    console.log(userDoc);
+  };
 app.listen(4000);
 // p8EkIgxY3F5bDYIL
 // s1kKMGLLnjgl1EIV
